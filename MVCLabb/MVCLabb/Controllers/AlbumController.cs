8d644bb814385e3b941852b4379e741c34adfdcd
Therese Sjögren <Therese@DESKTOP-UR5CQ7P>
2016-11-21 16:03:08 +0100

    added a new controller called album

---
+++ MVCLabb/MVCLabb/Controllers/AlbumController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLabb.Models;
+
+namespace MVCLabb.Controllers
+{
+    public class AlbumController : Controller
+    {
+        // GET: Album
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
