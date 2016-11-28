9a58c048f6ce44d94f1dde8e33e3f1f07539bbdd
Therese Sjögren <Therese@DESKTOP-UR5CQ7P>
2016-11-28 08:58:25 +0100

    something

--- MVCLabb/MVCLabb/Scripts/MyJavaScriptFile.js
+++ MVCLabb/MVCLabb/Scripts/MyJavaScriptFile.js
@@ -1,4 +1,5 @@
-﻿$(document).ready(function () {
+﻿//Upload picture
+$(document).ready(function () {
     var form = $("form");
     form.submit(function (e) {
         e.preventDefault();
@@ -6,6 +7,26 @@
             method: "POST",
             url: "/Gallery/UploadPicture",
             data: new FormData(document.getElementsByTagName("form")[0]), beforeSend: function () {
+                $("#imgSpinner1").show();
+            },
+            success: function (data) {
+                $("#imgSpinner1").hide();
+
+            },
+            processData: false,
+            contentType: false
+        });
+    });
+});
+//Delete picture
+$(document).ready(function () {
+    var form = $("form");
+    form.submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            method: "POST",
+            url: "/Gallery/DeletePicture",
+            data: new FormData(document.getElementsByID("form")[0]), beforeSend: function () {
                 $("#imgSpinner1").show();
             },
             success: function (data) {
