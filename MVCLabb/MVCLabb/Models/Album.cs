67e26c3be89724c2314437ca1fabb7d11af40fc7
Therese Sjögren <Therese@DESKTOP-UR5CQ7P>
2016-11-28 08:44:16 +0100

    added some jquery

--- MVCLabb/MVCLabb/Models/Album.cs
+++ MVCLabb/MVCLabb/Models/Album.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace MVCLabb.Models
     public class Album
     {
         public Guid AlbumID { get; set; }
+        [Required(ErrorMessage ="Must enter a name for the Album")]
         public string AlbumName { get; set; }
         public List<Comments> AlbumComment { get; set; }
         public List<Photo> Photos { get; set; }
