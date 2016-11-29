34881c0d691b21c654e1061c1d435890aafc9585
Therese Sjögren <Therese@DESKTOP-UR5CQ7P>
2016-11-29 15:14:55 +0100

    Photos done

--- MVCLabb/MVCLabbAjax/Controllers/GalleryController.cs
+++ MVCLabb/MVCLabbAjax/Controllers/GalleryController.cs
@@ -1,7 +1,9 @@
 ﻿using MVCLabbAjax.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -37,6 +39,53 @@ namespace MVCLabbAjax.Controllers
         {
             var p = photos.FirstOrDefault(x => x.PhotoID == id);
             p.PhotoComment.Add(new Comments { CommentOnPicture = photoComment });
+            return PartialView("Index",photos);
+        }
+        public ActionResult ShowImage(Guid id)
+        {
+            var showphoto = photos.FirstOrDefault(x => x.PhotoID == id);
+            return PartialView(showphoto);
+        }
+       
+        [HttpPost]
+        public ActionResult DeletePicture(Guid id, Photo photo)
+        {
+
+            var p = photos.FirstOrDefault(x => x.PhotoID == id);
+            string fullPath = Request.MapPath("~/Image/" + p.PhotoName);
+
+            if (System.IO.File.Exists(fullPath))
+            {
+                System.IO.File.Delete(fullPath);
+                //Session["DeleteSuccess"] = "Yes";
+                photos.Remove(p);
+            }
+            return RedirectToAction("Index",photos);
+        }
+        public ActionResult UploadPicture()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadPicture(string comment, HttpPostedFileBase[] files, Photo photo)
+        {
+            Thread.Sleep(3000);
+            if (!ModelState.IsValid)
+            {
+                return View(photo);
+            }
+            if (files == null)
+            {
+                ModelState.AddModelError("error", "Ingen Bild!");
+                return View(photo);
+            }
+            foreach (var file in files)
+            {
+                file.SaveAs(
+                Path.Combine(Server.MapPath("~/Image"), file.FileName));
+                photos.Add(new Photo { PhotoID = Guid.NewGuid(), PhotoName = file.FileName, PhotoComment = new List<Comments> { new Comments { CommentOnPicture = comment } } });
+            }
+
             return PartialView("Index",photos);
         }
     }
