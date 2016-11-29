bec4131c26a28add32da2fe4427f55cb9830167e
Therese Sjögren <Therese@DESKTOP-UR5CQ7P>
2016-11-29 15:28:15 +0100

    Testing the album

---
+++ MVCLabb/MVCLabbAjax/Controllers/AlbumController.cs
@@ -0,0 +1,28 @@
+﻿using MVCLabbAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLabbAjax.Controllers
+{
+    public class AlbumController : Controller
+    {
+        public static List<Album> albums = new List<Album>();
+        // GET: Album
+        public AlbumController()
+        {
+            if (!albums.Any())
+            {
+                albums.Add(new Album { AlbumID = Guid.NewGuid(), AlbumName = "WaterSports", Photos = new List<Photo>(), AlbumComment = new List<Comments> { new Comments { CommentOnAlbum = "Photos with different watersports" } } });
+                albums.Add(new Album { AlbumID = Guid.NewGuid(), AlbumName = "Boards", Photos = new List<Photo>(), AlbumComment = new List<Comments> { new Comments { CommentOnAlbum = "Photos on different kind of boards" } } });
+            }
+
+        }
+        public ActionResult Index()
+        {
+            return View(albums);
+        }
+    }
+}
