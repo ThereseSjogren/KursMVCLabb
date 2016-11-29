9c9492f70fd0804115afd962a93c5749786d813b
Therese Sjögren <Therese@DESKTOP-UR5CQ7P>
2016-11-29 13:19:59 +0100

    Added script

--- MVCLabb/MVCLabbAjax/Controllers/GalleryController.cs
+++ MVCLabb/MVCLabbAjax/Controllers/GalleryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCLabbAjax.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,20 @@ namespace MVCLabbAjax.Controllers
 {
     public class GalleryController : Controller
     {
+        public static List<Photo> photos = new List<Photo>();
         // GET: Gallery
+        public GalleryController()
+        {
+            if (!photos.Any())
+            {
+
+                photos.Add(new Photo { PhotoID = Guid.NewGuid(), PhotoName = "Skimboard.jpg", PhotoComment = new List<Comments> { new Comments { CommentOnPicture = "Skimboarding on the ocean" } } });
+                photos.Add(new Photo { PhotoID = Guid.NewGuid(), PhotoName = "SkimboardThree.jpg", PhotoComment = new List<Comments> { new Comments { CommentOnPicture = "Skimboarding of three" } } });
+                photos.Add(new Photo { PhotoID = Guid.NewGuid(), PhotoName = "SunsetSurf.jpg", PhotoComment = new List<Comments> { new Comments { CommentOnPicture = "Surfing in the sunset" } } });
+                photos.Add(new Photo { PhotoID = Guid.NewGuid(), PhotoName = "surf.jpg", PhotoComment = new List<Comments> { new Comments { CommentOnPicture = "Surfing on the ocean" } } });
+                photos.Add(new Photo { PhotoID = Guid.NewGuid(), PhotoName = "Water.jpg", PhotoComment = new List<Comments> { new Comments { CommentOnPicture = "Fine ocean" } } });
+            }
+        }
         public ActionResult Index()
         {
             return View();
